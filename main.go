f43ebfedc25a605a73dfba9aaa8085573a558eb4
petercyh <cyhuang@sg.ibm.com>
2020-08-20 16:45:12 +0800

    change hour

--- main.go
+++ main.go
@@ -35,25 +35,26 @@ func main() {
 	}
 	LOGGER.Infof("\tSuccessfully login!")
 
+	LOGGER.Debugf("%s", constant.Receiver)
 	for {
 		dt := time.Now()
 		theTime := dt.Format("01-02 15:04")
 		hour := strings.Split(strings.Split(theTime, " ")[1], ":")[0]
 		LOGGER.Debugf("Today is %s, It's %s o'clock now", strings.Split(theTime, " ")[0], hour)
-		if hour == "10" {
+		if hour == "02" {
 			date := strings.Split(strings.Split(theTime, " ")[0], "-")[0]
 
 			switch date {
 			case "1":
 				LOGGER.Infof("*** Time to pay the fibre fee, send message to the group ***")
-				err = bot.SendMessage(constant.PayInternetFeeMsg, "98103612@s.whatsapp.net")
+				err = bot.SendMessage(constant.PayInternetFeeMsg, constant.Receiver)
 				if err != nil {
 					LOGGER.Errorf("Failed to send message: %s", err.Error())
 					os.Exit(0)
 				}
 			case "14":
 				LOGGER.Infof("*** Time to pay the rental fee, send message to the group ***")
-				err = bot.SendMessage(constant.PayRentalMsg, "98103612@s.whatsapp.net")
+				err = bot.SendMessage(constant.PayRentalMsg, constant.Receiver)
 				if err != nil {
 					LOGGER.Errorf("Failed to send message: %s", err.Error())
 					os.Exit(0)
